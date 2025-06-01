8a2d9e3c7ea5d4602e31121e1682af52fcec5235
fwury <79013487+fwury@users.noreply.github.com>
2025-05-31 20:41:41 -0400

    Create ClinicProgram.java

---
+++ ClinicProgram.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+public class ClinicProgram {
+    public static void main(String[] args) {
+        File inputFile = new File("./input.txt");
+        
+        System.out.println("\nReading and processing data…");
+        Clinic clinic = new Clinic(inputFile);
+        
+        System.out.println("Writing report…");
+        String report = clinic.generateReport();
+        
+        File outputFile = new File("./output.txt");
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            writer.print(report);
+            System.out.println("Report written successfully.");
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error writing to output file: " + e.getMessage());
+        }
+    }
+}
